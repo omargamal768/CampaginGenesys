ae026c082d54ee4be90e3e53071d2d11f917beef
Omar <95372288+omargamal768@users.noreply.github.com>
2025-08-20 18:16:10 +0300

    start app

---
+++ Campagin/src/main/java/com/example/Campagin/CampaginApplication.java
@@ -0,0 +1,13 @@
+package com.example.Campagin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampaginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampaginApplication.class, args);
+	}
+
+}
